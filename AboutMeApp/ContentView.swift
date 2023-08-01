bca6e86cef252501393dfddb774328c7ae07f7de
Pearl <pearlnyax@gmail.com>
2023-08-01 09:31:24 +0100

    added a comment to the file

--- AboutMeApp/ContentView.swift
+++ AboutMeApp/ContentView.swift
@@ -6,18 +6,51 @@
 //
 
 import SwiftUI
-
+// comment
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+        
+        ZStack {
+
+        Color(red: 255/255, green: 178/255, blue: 102/255)
+        .ignoresSafeArea()
+
+        // add a Vstack to to center-align the image
+        VStack(alignment: .leading, spacing:20.0) {
+        Image("pearl")
+                        .resizable()
+               .frame(width: 360.0, height: 370.0)
+                        .cornerRadius(40)
+        // add the title and a small description
+        HStack(spacing: 20.0) {
+        Text("Pearl")
+        .underline()
+        .tracking(5)
+        .font(.title)
+        .fontWeight(.bold)
+        .foregroundColor(.green)
+
+
+        Text("KWK Scholar")
+        }// closes the Hstack
+
+        
+
+
+
+
+
+
+        }// closes the Vstack
+        }// closes the ZStack
         .padding()
-    }
-}
+                .background(Rectangle()
+                    .foregroundColor(.white))
+                        .cornerRadius(15)
+                        .shadow(radius: 15)
+                .padding()
+        }// closes the varbody
+        }//closes the ContentView
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
